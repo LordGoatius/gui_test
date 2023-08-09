584fddffc3137755a975354674176cb170c18f98
Jimmy Ostler <47489375+LordGoatius@users.noreply.github.com>
2023-08-09 16:13:49 -0600

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,57 @@
-use iced::theme::{self, Theme};
-use iced::widget::{button, column, text, row};
-use iced::{Alignment, Color, Element, Sandbox, Settings};
+use eframe::egui;
+use egui::{Visuals, Style};
 
-pub fn main() -> iced::Result {
-    Counter::run(Settings::default())
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Star Trek Picker",
+        options,
+        Box::new(|cc| {
+            let style = Style {
+                visuals: Visuals::dark(),
+                ..Style::default()
+            };
+            cc.egui_ctx.set_style(style);
+            Box::<MyApp>::default()
+        }),
+    )
 }
 
-#[derive(Default)]
-struct Counter {
-    theme: Theme,
-    value: i32,
-    value2: f64,
+struct MyApp {
+    button1: bool,
+    button2: bool,
+    button3: bool,
+    episode: String,
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    IncrementInt,
-    DecrementInt,
-    IncrementFloat,
-    DecrementFloat
-}
-
-impl Sandbox for Counter {
-    type Message = Message;
-
-    fn new() -> Self {
-        Self { theme: Theme::custom(theme::Palette {
-            background: Color::BLACK,
-            text: Color::from_rgb(0.8,0.8,0.8),
-            primary: Color::from_rgb(0.5, 0.5, 0.3),
-            success: Color::from_rgb(0.0, 1.0, 0.0),
-            danger:  Color::from_rgb(1.0, 0.0, 0.0),
-        }), value: 0, value2: 0.0 }
-    }
-
-    fn title(&self) -> String {
-        String::from("Counter - Iced")
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementInt => {
-                self.value += 1;
-            }
-            Message::DecrementInt => {
-                self.value -= 1;
-            }
-            Message::IncrementFloat => {
-                self.value2 += 0.1;
-            }
-            Message::DecrementFloat => {
-                self.value2 -= 0.1;
-            }
-
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            button1: false,
+            button2: false,
+            button3: false,
+            episode: "No Episode Picked".to_string()
         }
     }
+}
 
-    fn view(&self) -> Element<Message> {
-        row![
-            column![
-                button("Increment").on_press(Message::IncrementInt),
-                text(self.value).size(50),
-                button("Decrement").on_press(Message::DecrementInt),
-            ]
-            .padding(20)
-            .align_items(Alignment::Center),
-            column![
-                button("Inc Float").on_press(Message::IncrementFloat),
-                text(self.value2).size(50),
-                button("Dec Float").on_press(Message::DecrementFloat)
-            ]
-            .padding(20)
-            .align_items(Alignment::Center)
-        ]
-        .padding(20)
-        .into()
-    }
-
-    fn theme(&self) -> Theme {
-        self.theme.clone()
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Jimmy's Star Trek Picker");
+            ui.checkbox(&mut self.button1, "Button 1");
+            ui.checkbox(&mut self.button2, "Button 2");
+            ui.checkbox(&mut self.button3, "Button 3");
+            ui.horizontal(|ui| {
+                if ui.button("Generate Episode").clicked() {
+                    self.episode = format!("Episode is: {}",5);
+                }
+                ui.label(&self.episode);
+            });
+        });
     }
 }
